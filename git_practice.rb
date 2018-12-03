dbf2ccc6fd1e0598caca6daccefef8419e88b76b
Peregrine Balas <peregrinereedbalas@gmail.com>
2018-12-02 21:53:18 -0700

    hash practice

--- git_practice.rb
+++ git_practice.rb
@@ -1,27 +1,34 @@
-numbers = []
-counter = 0
-loop do
-  counter += 1
-  numbers << counter
-  if counter >= 500
-    break
-  elsif counter % 3 == 0
-    numbers << counter
-    counter += 2
-    numbers << counter
-  elsif counter > 3
-    numbers << counter
-    counter *= 2
-    numbers << counter
-    counter -= 7
-    numbers << counter
-  elsif numbers.length == 88
-    break
+hash = {
+  A1: 'x',
+  A2: 'x',
+  A3: 'x',
+  A4: 'x',
+  B1: 'x',
+  B2: 'x',
+  B3: 'x',
+  B4: 'x',
+  C1: 'x',
+  C2: 'x',
+  C3: 'x',
+  C4: 'x'
+}
+
+a = []
+b = []
+c = []
+
+hash.each do |key, value|
+  if key.to_s[0] == 'A'
+    a << value
+  elsif key.to_s[0] == 'B'
+    b << value
+  elsif key.to_s[0] == 'C'
+    c << value
   end
 end
-numbers.each do |number|
-  p number
-end
-numbers.reverse.each do |number|
-  p number
-end
+print a.join(" ")
+print "\n"
+print b.join(" ")
+print "\n"
+print c.join(" ")
+print "\n"
